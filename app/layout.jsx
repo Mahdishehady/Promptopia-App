7a0b35d040549e89f9ad2615aef03a10d2fa4cb2
Mahdishehady <shehadi2000@gmail.com>
2023-10-26 23:44:01 +0300

    adding clarity

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css"
 import Provider from "@components/Provider";
 import Nav from "@components/Nav";
+import Script from 'next/script';
 
 
 export const metadata = {
@@ -11,6 +12,13 @@ export const metadata = {
 const RootLayout = ({ children }) => (
   <html lang='en'>
     <body>
+    <Script type="text/javascript">
+    {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "jgs7s7eg1i");`}
+</Script>
       <Provider>
         <div className='main'>
           <div className='gradient' />
